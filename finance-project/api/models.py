ecac7a759114e27c70194295841fa2898c6c2eb9
andraiordand <andraiordan24@gmail.com>
2023-03-28 18:25:09 +0300

    Add api endpoints for read/create user

---
+++ finance-project/api/models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+
+
+class UserAdd(BaseModel):
+    username: str = Field(description="Alphanumeric username between 6 and 20 chars")
+
+
+class UserInfo(BaseModel):
+    username: str
+    stocks: list[str]
+
+    class Config:
+        orm_mode = True
